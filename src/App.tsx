9dd871928aa031d01d74aa411c22b6ac46ca95b2
Soo Jee Park <talktosoojee@gmail.com>
2022-09-04 14:00:41 -0700

    changed Home to About

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
+import About from "./components/About";
 import Projects from "./components/Projects";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -14,7 +14,7 @@ function App() {
         <div className="font-mono bg-red-50 min-h-screen p-6">
           <Navbar />
           <Routes>
-            <Route path="/about" element={<Home />} />
+            <Route path="/about" element={<About />} />
             <Route path="/projects" element={<Projects />} />
             <Route path="*" element={<Welcome />} />
           </Routes>
