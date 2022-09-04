e7fd28c15b595d89d146d4019a5e6f557f49238b
Soo Jee Park <talktosoojee@gmail.com>
2022-09-04 13:45:44 -0700

    changed routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./components/Home";
 import Projects from "./components/Projects";
 import Navbar from "./components/Navbar";
@@ -13,8 +13,9 @@ function App() {
         <div className="font-mono bg-red-50 min-h-screen p-6">
           <Navbar />
           <Routes>
-            <Route default path="/portfolio-website" element={<Home />} />
+            <Route path="/about" element={<Home />} />
             <Route path="/projects" element={<Projects />} />
+            <Route path="*" element={<Navigate to="/about" replace />} />
           </Routes>
           <Footer />
         </div>
